8b96c261c5a8f3da5326e8a4e69ffc3c08c28645
Roberto <rorepoid@gmail.com>
2020-06-10 15:48:35 -0500

    test: Root route test

---
+++ test/app.test.js
@@ -0,0 +1,19 @@
+const app = require("../app");
+const chai = require("chai");
+const http = require("chai-http");
+
+const { expect } = chai;
+chai.use(http);
+describe("App", () => {
+    describe("GET /", () => {
+        it("Returns Hello World", (done) => {
+            chai.request(app)
+                .get("/")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.text).to.equals("Hello World")
+                    done();
+              });
+        });
+    });
+});
